850d465e387adf989da7e9f9b6cc6ac7f18a1d0d
YE Cheng Wei <a0919958057@gmail.com>
2018-05-09 22:13:41 +0800

    Add new serial library test program, and fix include problem

--- src/test/test/test.cxx
+++ src/test/test/test.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "serial/serial.h"
 
 using namespace std;
 
@@ -7,6 +8,17 @@ int main(int argc, char const *argv[]) {
     if(argc < 2) {
         cout << "Usage: --find-devices --help --version";
     }
+    vector<serial::PortInfo> devices_found = serial::list_ports();
+
+	vector<serial::PortInfo>::iterator iter = devices_found.begin();
+
+	while( iter != devices_found.end() )
+	{
+		serial::PortInfo device = *iter++;
+
+		printf( "(%s, %s, %s)\n", device.port.c_str(), device.description.c_str(),
+     device.hardware_id.c_str() );
+	}
 
     return 0;
 }
