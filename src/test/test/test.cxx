d12a192d6d24473885fc16053f33a39c07065dbf
YE Cheng Wei <a0919958057@gmail.com>
2018-05-09 21:10:29 +0800

    init commit

---
+++ src/test/test/test.cxx
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+    if(argc < 2) {
+        cout << "Usage: --find-devices --help --version";
+    }
+
+    return 0;
+}
